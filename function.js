240d3e294d71a2ed09ed54f7fc433b904261f572
Soichiro Yoshimura <yoshimura@soichiro.org>
2017-07-14 14:32:28 +0900

    Update function.js

--- function.js
+++ function.js
@@ -1,6 +1,6 @@
 (function(){
     'use strict';
-    var myBirthTime = new Date(1982, 12, 17, 12, 30);
+    var myBirthTime = new Date(1982, 11, 17, 12, 30);
     function updateParagraph(){
         var now = new Date();
         var seconds = (now.getTime() - myBirthTime.getTime()) / 1000;
